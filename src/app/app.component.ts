817ff45dcc7837aaf5da61fe4ff51b49343df302
Benjamin Milčić <benjamin.milcic@gmail.com>
2024-11-15 12:01:23 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { CdkDrag } from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatDialogModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'drag-test';
+  readonly dialog = inject(MatDialog);
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+ 
 }
+
+ @Component({
+   selector: 'dialog-content-example-dialog',
+   templateUrl: './dialog-content-example-dialog.html',
+   standalone: true,
+   imports: [MatDialogModule, MatButtonModule,CdkDrag],
+ })
+ export class DialogContentExampleDialog {}
